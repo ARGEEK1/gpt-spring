0094e513e2532556e0a6fbfbaabfb73a6f96ee90
ARGEEK1 <antonio198889@gmail.com>
2023-09-03 15:13:42 +0200

    update

---
+++ src/main/java/com/practica/gptspring/GptSpringApplication.java
@@ -0,0 +1,13 @@
+package com.practica.gptspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GptSpringApplication.class, args);
+	}
+
+}
